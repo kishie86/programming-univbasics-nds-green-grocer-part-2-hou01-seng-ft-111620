b3cc8dc76c48db34c0ad20cb073582415f5b764e
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-14 00:35:31 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -34,11 +34,12 @@ end
 def apply_clearance(cart)
   counter = 0 
   while counter < cart.length
-  if cart[counter][:clearance]
+    if cart[counter][:clearance]
     cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.2)).round(2)
+    end
+   counter += 1
   end
-  end
-  counter += 1
+ cart
 end
   
   # Consult README for inputs and outputs
